47cd127f1658fdfbeb4d6831a8b0e4e0937de5ed
Viktor Tunev <viktorvladimiro@mail.ru>
2020-10-22 23:25:51 +0200

    Functionality implemented

--- obb/OBB.py
+++ obb/OBB.py
@@ -1,17 +1,1 @@
-class OBB():
-
-    def __init__(self, data):
-
-        self.data = list()
-        for point in data:
-            if len(point) != 3:
-                raise RuntimeError("A point must have the format (X,Y,Z)")
-            self.data.append(point)
-
-    def get(self):
-        width = 0
-        height = 0
-        depth = 0
-        trafo = [None]
-
-        return (width, height, depth, trafo)
+from pyobb.obb import OBB
--- reader/Reader.py
+++ reader/Reader.py
@@ -2,20 +2,48 @@ from OBB import OBB
 
 class Reader():
 
-    def __init__(self, file):
+    def __init__(self, file, precision=3):
 
-        self.data = list()
-        # read and parse the file here
+        self.precision = int(precision)
+        if self.precision < 0:
+            self.precision = 0
+        self.format = '{:.' + str(self.precision) + 'f}'
+
+        self.points = list()
+        with open(file,'r') as f:
+            eof = False
+            while not eof:
+                point = f.readline()
+                if not point.endswith('\n'):
+                    eof = True
+                if len(point.strip()) == 0:
+                    continue
+                point = point.split()
+                try:
+                    point = [int(coord) for coord in point]
+                except:
+                    raise RuntimeError('Incorrect input file format. Only numerical values accepted')
+                if len(point) != 3:
+                    raise RuntimeError('Input points must have the format (X,Y,Z)')
+                self.points.append(point)
+        if len(self.points) < 2:
+            raise RuntimeError('At least 2 points must be present in the input file')
+
 
     def print_obb_params(self):
 
-        obb = OBB(self.data)
-        result = obb.get()
+        obb = OBB.build_from_points(self.points)
 
         print('The obb is:')
-        print('Width: {}'.format(result[0]))
-        print('Height: {}'.format(result[1]))
-        print('Depth: {}'.format(result[2]))
-        print('Trafo: {}'.format(' '.join([str(x) for x in result[3]])))
+        print(('Width: ' + self.format).format(obb.extents[0]))
+        print(('Height: ' + self.format).format(obb.extents[1]))
+        print(('Depth: ' + self.format).format(obb.extents[2]))
+        trafo = list()
+        for coord in obb.centroid:
+            trafo.append(coord)
+        for row in obb.rotation:
+            for elem in row:
+                trafo.append(elem)
+        print('Trafo: {}'.format(' '.join([self.format.format(x) for x in trafo])))
 
 
